3041e4ea7e7ac7340a3685b3a75690f24569ccef
MarceloEnrike <marcelogota@gmail.com>
2026-05-21 20:26:03 -0300

    Fix: corrijo endpoint /marcas

--- app.py
+++ app.py
@@ -20,21 +20,11 @@ def motos():
     return jsonify({"status": "Ok"})
 
 # GET - listar marcas
-#@app.route("/marcas", methods=["GET"])
-#def get_marcas():
-#forzamos un error de codigo
-    #return jsonify(motos())
-#    return jsonify(marcas)
-
 @app.route("/marcas", methods=["GET"])
 def get_marcas():
-    raise Exception("Error intencional para test")
-
-def test_get_marcas():
-    client = app.test_client()
-    response = client.get("/marcas")
-
-    assert response.status_code == 200
+    return jsonify(marcas)
+#forzamos un error de codigo
+#    raise Exception("Error intencional para test")
 
 # POST - crear marca
 @app.route("/marcas", methods=["POST"])
